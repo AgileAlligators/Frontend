eb4bf61d390633ddbb66a946b058567efd83051d
baris2 <ceralest55@gmail.com>
2022-06-20 18:42:17 +0200

    add filter, selection and diagram section to guide

--- src/views/Guide.vue
+++ src/views/Guide.vue
@@ -2,26 +2,58 @@
   <AAView class="view-guide">
     <AASection title="Ladungsträger filtern">
       <p>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Esse possimus
-        a quia dicta repellat? Itaque, ea autem quos consectetur rem obcaecati
-        debitis ratione unde iste, enim eum ab, placeat recusandae.
+        Die aufgelisteten Ladungsträger beim Dashboard können nach Wunsch
+        gefiltert werden. Hierzu klick man auf das Filtersymbol, welches über
+        der Liste von Ladungträgern zu finden ist.
       </p>
-      <!-- <img src="guide/NAME.png" alt=""> -->
+      <img src="../../public/assets/guide/FilterIcon.png" />
+      <p>
+        Daraufhin öffnet sich ein Fenster, dass die aktuellen Einstellung des
+        Filters darstellt. Soll ein Filter hinzugefügt werden, kann man auf eine
+        der Filteroptionen klicken. Dies öffnet ein Dropdown-Menü, bei dem man
+        per Klick auswählen kann, welche Untergruppe angezeigt werden soll. Ist
+        eine Option ausgewählt, so wird diese grün gekennzeichnet. Dies
+        bedeutet, dass Ladungsträger mit dieser Eigenschaft anzeigt werden,
+        falls diese Ladungsträger vorhanden sind. In der folgenden Abbildung
+        sind alle möglichen Bauteiloptionen ausgewählt und deshalb werden alle
+        vorhanden Ladungsräger, unabhängig vom Bauteil dargestellt.
+      </p>
+      <img src="../../public/assets/guide/FilterOverlay.png" />
     </AASection>
     <AASection title="Ladungsträger auswählen">
       <p>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corrupti
-        dignissimos beatae labore nihil eius non deleniti quod exercitationem
-        suscipit qui totam omnis accusamus voluptas id, culpa ut itaque officiis
-        tenetur?
+        Trotz der Filterung kann die Situation entstehen, bei dem man sich nur
+        für einen Ladungsträger interessiert. Hier kann der Benutzer durch
+        setzen eines Hakens einzelne Ladungsträger auswählen, die von Interesse
+        sind. Nachdem die Auswahl der Ladungsträgern getroffen wurde, kann man
+        via Navigationsleiste auf die Diagrammansicht wechseln. Eigenschaften
+        wie Beladung, Standzeiten und Erschütterungen werden in Form von
+        Liniendiagrammen dargestellt. Werden mehrere Ladungsträger ausgewählt,
+        so werden die Daten der Ladungsträger farblich markiert. Sind keine
+        Haken gesetzt, verhält sich das System so, als wurden alle Ladungsträger
+        ausgewählt. Daraufhin wird ein Durchschnitt zwischen den Daten der
+        verschiedenen Ladungsträger berechnet. Im folgenden Beispiel wurden drei
+        Ladungsträger ausgewählt.
       </p>
-    </AASection>
-    <AASection title="Ladungsträger auswählen">
+      <img src="../../public/assets/guide/CarrierSelection.png" />
+      <p>Das entsprechende Beladungdiagramm sieht wie folgt aus.</p>
+      <img src="../../public/assets/guide/LoadDiagram.png" />
       <p>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corrupti
-        dignissimos beatae labore nihil eius non deleniti quod exercitationem
-        suscipit qui totam omnis accusamus voluptas id, culpa ut itaque officiis
-        tenetur?
+        Zusätzlich kann die angezeigt Zeitspanne angepasst werden. Im folgenden
+        Beispiel wurde der Zeitraum auf 22.06.22 - 26.06.22 eingeschränkt.
+      </p>
+      <img src="../../public/assets/guide/LimitedLoadDiagram.png" />
+      <p>
+        Analog zu der Diagrammansicht, findet man unter dem Reiter "Hotspots"
+        Hotspots-Diagramme, die sich auf die ausgewählten Ladungsträger
+        beziehen. Sie visualisieren verschiedene Verhalten der ausgewählten
+        Ladungsträger wie Bewegungsmuster, Standzeitentwicklung,
+        Beladungsentwicklung und Vibrationen.
+      </p>
+      <img src="../../public/assets/guide/CarrierMovement.png" />
+      <p>
+        Wählt man hier einen Zeitraum aus und klickt auf das Startsymbol, so
+        werden die Bewegungen der Ladungsträger visuell abgespielt.
       </p>
     </AASection>
   </AAView>
