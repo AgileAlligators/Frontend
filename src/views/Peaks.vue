38cfb1b8bd9a540229dd6dd4b744c5514d193c10
Timo Scheuermann <timo.scheuermann@gmx.de>
2022-06-26 14:22:23 +0200

    .

--- src/views/Peaks.vue
+++ src/views/Peaks.vue
@@ -1,17 +1,20 @@
 <template>
   <AAView class="view-peaks">
+    <AATimeRange slot="static" />
+
     <AAIdlePeak />
     <AAVibrationPeak />
   </AAView>
 </template>
 
 <script lang="ts">
+import AATimeRange from '@/components/AATimerange.vue';
 import AAView from '@/components/AAView.vue';
 import AAIdlePeak from '@/components/peaks/AAIdlePeak.vue';
 import AAVibrationPeak from '@/components/peaks/AAVibrationPeak.vue';
 import { Vue, Component } from 'vue-property-decorator';
 
-@Component({ components: { AAView, AAVibrationPeak, AAIdlePeak } })
+@Component({ components: { AAView, AATimeRange, AAVibrationPeak, AAIdlePeak } })
 export default class Peaks extends Vue {}
 </script>
 
